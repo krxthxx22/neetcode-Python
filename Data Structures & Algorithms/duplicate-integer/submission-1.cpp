c1cd9b66d4772195b210621a0c0bfb5b5a70eb44
Khiruthiha MS <156287797+krxthxx22@users.noreply.github.com>
2026-06-09 14:56:18 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+  
+    unordered_set<int> set;
+    
+    for(int i = 0; i<nums.size();i++){
+        set.insert(nums[i]);
+    }
+    
+    // if it contains duplicate then the size of 
+    // set will not equal to the length of the array
+    if(set.size() < nums.size())
+        return true;
+    
+    return false;
+
+    }
+};
