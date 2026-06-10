059cd6b5a9010138e7782ae992f53b87d23049b8
Khiruthiha MS <156287797+krxthxx22@users.noreply.github.com>
2026-06-10 18:54:07 +0530

    Add: python-list-slicing - submission-0

---
+++ Python For Beginners/python-list-slicing/submission-0.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def get_last_three_elements(my_list: List[int]) -> List[int]:
+    length = len(my_list)
+    x = length -3
+    return my_list[x:]
+
+        
+
+
+# do not modify below this line
+print(get_last_three_elements([1, 2, 3]))
+print(get_last_three_elements([1, 2, 3, 4, 5]))
+print(get_last_three_elements([1, 2, 3, 4, 5, 6, 7, 8, 9]))
